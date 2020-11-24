649b7159231150316bc30e8b9e8a03260ecfe9a0
Dener Cirilo Fontes <dener.fontes@gmail.com>
2020-11-24 05:32:19 -0300

    configuracao inicial com react + webpack + babel

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+console.log('react', React)
+
+const App = () => {
+  return (
+    <h1 className="alo">Olá Mundo</h1>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist', 'main.js'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+}
